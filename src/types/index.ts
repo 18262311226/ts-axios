68ee86ce544f26d5db5260ff5dd9fb44451effa8
无为 <3435516833@qq.com>
2022-05-24 21:59:50 +0800

    添加拦截器

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,17 @@
 export type Method = 'get' | 'GET' | 'delete' | 'DELETE' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
-    url: string
+    url?: string
     method?: Method
     data?:any
     params?:any
     headers?:any
-    responseType: XMLHttpRequestResponseType
+    responseType?: XMLHttpRequestResponseType
     timeout?:number
 }
 
-export interface AxiosResponse{
-    data:any
+export interface AxiosResponse<T=any>{
+    data:T
     status:number
     statusText:string
     headers: any
@@ -19,7 +19,7 @@ export interface AxiosResponse{
     request:any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse>{
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>>{
 
 }
 
@@ -29,4 +29,45 @@ export interface AxiosError extends Error{
     code?:string | null
     request?:any
     response?: AxiosResponse
+}
+
+export interface Axios{
+    interceptors:{
+        request:AxiosInterceptorManager<AxiosRequestConfig>
+        response: AxiosInterceptorManager<AxiosResponse>
+    }
+    
+    request<T=any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+    get<T=any>(url:string, config: AxiosRequestConfig):AxiosPromise<T>
+
+    delete<T=any>(url:string, config: AxiosRequestConfig):AxiosPromise<T>
+
+    head<T=any>(url:string, config: AxiosRequestConfig):AxiosPromise<T>
+
+    options<T=any>(url:string, config: AxiosRequestConfig):AxiosPromise<T>
+
+    post<T=any>(url:string, data:any, config:AxiosRequestConfig): AxiosPromise<T>
+
+    put<T=any>(url:string, data:any, config:AxiosRequestConfig): AxiosPromise<T>
+
+    pacth<T=any>(url:string, data:any, config:AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInstance{
+    <T=any>(config:AxiosRequestConfig): AxiosPromise<T>
+    <T=any>(url:string, config?: AxiosRequestConfig):AxiosPromise<T>
+}
+
+export interface AxiosInterceptorManager<T>{
+    use(resolved:ResolvedFn<T>,rejected?:RejectedFn):number
+    eject(id:number):void
+}
+
+export interface ResolvedFn<T>{
+    (val:T): T | Promise<T>
+}
+
+export interface RejectedFn{
+    (error:any):any
 }
